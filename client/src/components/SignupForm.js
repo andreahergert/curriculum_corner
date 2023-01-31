0a171d6ee797d024dba9b2e41d23d2bbe16bdfd8
Tahlia <tbolden13@gmail.com>
2023-01-31 15:31:12 -0800

    editing typos in index.css

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -7,7 +7,7 @@
 	</div>
 	<div class="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
 		<div>
-			<h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Create A Curriculum Corner Account </h1>
+			<h1 class="text-3xl font-bold text-center mb-4 cursor-pointer">Curriculum Corner Account</h1>
 			<p class="w-80 text-center text-sm mb-8 font-semibold text-gray-700 tracking-wide cursor-pointer">Create an
 				account to start your coding journey</p>
 		</div>
