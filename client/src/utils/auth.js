f1b99f5ace56ad7c452e35c9d35b7cf4d48df315
Andrea Hergert <ahergert24@hotmail.com>
2023-01-30 20:40:10 -0800

    added auth file deleted localstorage file

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,2 +1,39 @@
-// auth
-// use this to decode a token and get the user's information out of it
+import decode from "jwt-decode";
+
+class AuthService {
+  getProfile() {
+    return decode(this.getToken());
+  }
+
+  loggedIn() {
+    const token = this.getToken();
+    // If there is a token and it's not expired, return `true`
+    return token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  isTokenExpired(token) {
+    const decoded = decode(token);
+    if (decoded.exp < Date.now() / 1000) {
+      localStorage.removeItem("id_token");
+      return true;
+    }
+    // If token hasn't passed its expiration time, return `false`
+    return false;
+  }
+
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    window.location.reload();
+  }
+}
+
+export default new AuthService();
