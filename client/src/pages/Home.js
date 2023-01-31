bf97e58972cdd9165c8990f965fc3b567f7c614c
Gator <allidodt@gmail.com>
2023-01-30 21:25:31 -0800

    login and sign up pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,1 +1,2 @@
-// Home
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
--- client/src/pages/HomeLoggedin.js
+++ client/src/pages/HomeLoggedin.js
@@ -1,1 +1,3 @@
 // Home Loggedin 
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
