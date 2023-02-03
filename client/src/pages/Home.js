58d1d2047bd271e43bb9558f7f5c4624d0917faf
yazzy14 <yazminbazail@icloud.com>
2023-02-02 17:06:52 -0800

    removed tailwind from index.html

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useQuery } from "apollo/client";
+
+const todoForm = () => {
+  const []
+}
+
 
 const Home = () => {
+  const { loading, data } = useQuery(QUERY_TODOS);
+  const todos = data?.todos || [];
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h3>Todo's</h3>
+
+    </div>
+  );
+};
 
 export default Home;
