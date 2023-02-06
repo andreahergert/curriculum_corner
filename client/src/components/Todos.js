d752cef60fdc3a8acdaadd30ee5c83f4fe57698a
Andrea Hergert <ahergert24@hotmail.com>
2023-02-05 16:26:51 -0800

    added complete and remove buttons, not working fully yet though and need styling

--- client/src/components/Todos.js
+++ client/src/components/Todos.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Todos = ({ todos, title }) => {
+const Todos = ({ todos, title, handleRemoveTodo }) => {
   if (!todos.length) {
     return <h3>Add Todo's</h3>;
   }
@@ -24,7 +24,13 @@ const Todos = ({ todos, title }) => {
             </div>
             
             <div className="">
-            <button classname="" value={todo.completed}>{/*I dont know if the value is correct or what exactly we would need to put for the btn*/}</button>
+            <button 
+                className=""
+                onClick={() => handleRemoveTodo(todo._id)}
+              >
+                Remove Todo
+              </button>
+            <button className="" value={todo.completed}>{/*I dont know if the value is correct or what exactly we would need to put for the btn*/}Completed</button>
             </div>
             </div>
             <Link
